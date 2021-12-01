91d619284070109e7c42fd0ed69a420d4c74653b
louis <louis@jod.li>
2021-12-01 07:29:55 +0100

    Fixed some warnings in test.

--- test/matomo_forever_test.dart
+++ test/matomo_forever_test.dart
@@ -1,24 +1,23 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:matomo_forever/matomo_forever.dart';
 
 void main() {
-  const String site_url = String.fromEnvironment("site_url");
-  const String token_auth = String.fromEnvironment("token_auth");
+  const String siteUrl = String.fromEnvironment("site_url");
+  const String tokenAuth = String.fromEnvironment("token_auth");
   const String idsite = String.fromEnvironment("idsite");
-  const String action_name = String.fromEnvironment("action_name");
+  const String actionName = String.fromEnvironment("action_name");
 
   test('intialize', () {
     MatomoForever.init(
-      site_url,
+      siteUrl,
       int.tryParse(idsite) ?? 0,
-      tokenAuth: token_auth,
+      tokenAuth: tokenAuth,
     );
     expect(MatomoForever.isInitialized, true, reason: "Not initialized");
   });
 
   test('track', () async {
-    expect(await MatomoForever.track(action_name), true,
+    expect(await MatomoForever.track(actionName), true,
         reason: "Couldn't track");
   });
 }
